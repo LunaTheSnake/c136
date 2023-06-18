bcf7a3f809c13d0cc3275d64c512f6f4ab25955c
LunaTheSnake <109224823+LunaTheSnake@users.noreply.github.com>
2023-06-18 20:51:53 -0300

    Add files via upload

---
+++ output2.py
@@ -0,0 +1,17 @@
+{
+    "data": {
+        "distance_from_earth": "305.0",
+        "distance_from_their_sun": "1.29 AU",
+        "gravity": 413.7736760058701,
+        "name": "11 Comae Berenices b",
+        "orbital_period": "326 days",
+        "orbital_speed": 430.27845944103615,
+        "planet_mass": 6165.32,
+        "planet_radius": 12.096,
+        "planet_type": "Gas Giant",
+        "specifications": [
+            "goldilock"
+        ]
+    },
+    "message": "success"
+}
